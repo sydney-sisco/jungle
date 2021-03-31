bf88f627c7a520cc10ef171189dacf19b5f1c218
Sydney Sisco <hello@sydsis.co>
2021-03-31 12:14:18 -0700

    create session controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,11 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
-  # These routes will be for signup. The first renders a form in the browser, the second will 
-  # receive the form and create a user in our database using the data given to us by the user.
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
