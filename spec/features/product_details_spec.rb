e7bb252dd3d0e6272fc8378019169a73c5a7cefc
Sydney Sisco <hello@sydsis.co>
2021-04-06 15:29:19 -0700

    Feature spec for product show page

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+RSpec.feature "Visitor navigates to the product detail page", type: :feature, js: true do
 
   # SETUP
   before :each do
@@ -18,14 +18,15 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
+  scenario "They see products details" do
     # ACT
     visit root_path
+    first('article.product').find_link('Details').click()
 
     # DEBUG
     # save_screenshot
 
     # VERIFY
-    expect(page).to have_css 'article.product', count: 10
+    expect(page).to have_css 'body > main > section > article > div > div.col-sm-4 > img', count: 1
   end
 end
